f54495f29644ccf763edcf00d08f1ee5a3492b65
Oyewale <oyewalefavour424@gmail.com>
2024-05-08 19:51:36 +0100

    utilized the getrestaurantbannerimages

--- lib/view/registrationscreen/restaurant_reg_screen.dart
+++ lib/view/registrationscreen/restaurant_reg_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:developer';
 
+import 'package:carousel_slider/carousel_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:kfc_restaurant/controller/provider/restaurant_register_provider.dart';
 import 'package:kfc_restaurant/controller/services/image_services.dart';
 import 'package:kfc_restaurant/view/registrationscreen/widgets/restaurant_reg_widgets.dart';
+import 'package:provider/provider.dart';
 
 class RestaurantRegistrationScreen extends StatefulWidget {
   const RestaurantRegistrationScreen({super.key});
@@ -18,18 +21,25 @@ class _RestaurantRegistrationScreenState
       TextEditingController();
   final TextEditingController _restaurantNameController =
       TextEditingController();
+  final CarouselController _carouselController = CarouselController();
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
         child: RestaurantRegistrationWidget(
       onTapped: () async {
-        await ImageServices.getImagesFromGallery(context: context);
+        // await ImageServices.getImagesFromGallery(context: context);
+
+        // await ImageServices.
+        await context
+            .read<RestaurantRegisterProvider>()
+            .getRestaurantBannerImages(context);
 
         log("Clicked on the Images container");
       },
       restaurantLicenseController: _restaurantLicensceController,
       restaurantNameController: _restaurantNameController,
+      carouselController: _carouselController,
     ));
   }
 }
--- lib/view/registrationscreen/widgets/restaurant_reg_widgets.dart
+++ lib/view/registrationscreen/widgets/restaurant_reg_widgets.dart
@@ -1,19 +1,26 @@
+import 'dart:io';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:kfc_restaurant/controller/provider/restaurant_register_provider.dart';
 import 'package:kfc_restaurant/utils/custom_textfield.dart';
+import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
 
 class RestaurantRegistrationWidget extends StatelessWidget {
   final TextEditingController restaurantNameController;
   final TextEditingController restaurantLicenseController;
   final VoidCallback onTapped;
+  final CarouselController carouselController;
 
   const RestaurantRegistrationWidget({
     super.key,
     required this.restaurantNameController,
     required this.restaurantLicenseController,
     required this.onTapped,
+    required this.carouselController,
   });
 
   @override
@@ -28,45 +35,84 @@ class RestaurantRegistrationWidget extends StatelessWidget {
           SizedBox(
             height: 2.h,
           ),
-          GestureDetector(
-            onTap: onTapped,
-            child: Container(
-              height: 20.h,
-              width: 94.w,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(5.sp),
-                color: Colors.grey.shade300,
-              ),
-              child: Column(
-                children: [
-                  SizedBox(
-                    height: 4.h,
+          Consumer<RestaurantRegisterProvider>(
+              builder: (context, registrationProvider, child) {
+            if (registrationProvider.restaurantBannerImages.isEmpty) {
+              return GestureDetector(
+                onTap: onTapped,
+                child: Container(
+                  height: 20.h,
+                  width: 94.w,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5.sp),
+                    color: Colors.grey.shade300,
                   ),
-                  Container(
-                    height: 5.h,
-                    width: 5.h,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(color: Colors.black),
-                    ),
-                    child: const Center(
-                      child: FaIcon(
-                        FontAwesomeIcons.plus,
-                        // color: Colors.white,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 4.h,
                       ),
-                    ),
+                      Container(
+                        height: 5.h,
+                        width: 5.h,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(color: Colors.black),
+                        ),
+                        child: const Center(
+                          child: FaIcon(
+                            FontAwesomeIcons.plus,
+                            // color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 1.h),
+                      Text(
+                        "Add",
+                        style: TextStyle(
+                          fontSize: 12.sp,
+                        ),
+                      )
+                    ],
                   ),
-                  SizedBox(height: 1.h),
-                  Text(
-                    "Add",
-                    style: TextStyle(
-                      fontSize: 12.sp,
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
+                ),
+              );
+            } else {
+              List<File> bannerImages =
+                  registrationProvider.restaurantBannerImages;
+              return Container(
+                height: 23.h,
+                width: 94.w,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(5.sp),
+                  border: Border.all(color: Colors.grey),
+                ),
+                child: CarouselSlider(
+                  carouselController: carouselController,
+                  options: CarouselOptions(
+                    height: 23.h,
+                    autoPlay: true,
+                    viewportFraction: 1,
+                  ),
+                  items: bannerImages
+                      .map((image) => ClipRRect(
+                            borderRadius: BorderRadius.circular(7.0),
+                            child: Container(
+                              width: 94.w,
+                              decoration: BoxDecoration(
+                                color: Colors.red,
+                                image: DecorationImage(
+                                  image: FileImage(image),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                          ))
+                      .toList(),
+                ),
+              );
+            }
+          }),
           SizedBox(
             height: 4.h,
           ),
